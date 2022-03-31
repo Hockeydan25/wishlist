2199fc40d618f5ecdba398a1b6eb48b3ac85d988
Dan Smestad <hockeydan25@gmail.com>
2022-03-31 10:17:07 -0500

    edits to css files in a static folder to formatt. added to the test file one test to test if wish list is empty.

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Place
+# Create your tests here. reverse extention of python testcase 
+
+class TestHomePage(TestCase):
+
+    def test_home_page_shows_empty_list_message_for_empty_database(self):  # test method when th list is empty 
+        home_page_url = reverse('place_list')  # url we are checking for a response
+        response = self.client.get(home_page_url)  # 
+        self.assertTemplateUsed(response,'travel_wishlist/wishlist.html')  # template that should be used to check
+        self.assertContains(response, 'You have no places in your wish list')  # must exact case and characters!
+        # djamgo will destroy data in database 
