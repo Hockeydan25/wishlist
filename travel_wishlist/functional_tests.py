4f70174991f3c941d392af7331b01e7ee9cd59c2
Dan Smestad <hockeydan25@gmail.com>
2022-04-01 11:16:01 -0500

    modified tests add last testing code. driver added to enviromental path. path not ref while running tests need to correct. class updated to AddPlacesTest

--- travel_wishlist/functional_tests.py
+++ travel_wishlist/functional_tests.py
@@ -21,7 +21,7 @@ class TitleTest(LiveServerTestCase):
         self.assertIn('Travel Wishlist', self.selenium.title)    
 
 
-class AddPLaces(LiveServerTestCase):
+class AddPlacesTest(LiveServerTestCase):
 
     fixtures = ['test_places']
 
@@ -29,7 +29,7 @@ class AddPLaces(LiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(10)
+        cls.selenium.implicitly_wait(10)  # waits 10 seconds for loading of page giving a 
 
     @classmethod
     def tearDownClass(cls):
@@ -39,11 +39,20 @@ class AddPLaces(LiveServerTestCase):
     def test_add_new_place(self):
 
         self.selenium.get(self.live_server_url)
-        input_name = self.selenium.find_element_by_id('id_name')
+        input_name = self.selenium.find_element_by_id('id_name')  # wishlist html input line puts denver in auto 
         input_name.send_keys('Denver')
 
-        add_button = self.selenium.find_element_by_id('add-new-place')
-        add_button.click()
+        add_button = self.selenium.find_element_by_id('add-new-place')  # wishlist html button id 
+        add_button.click()  # auto test will click button to add denver
 
-        denver = self.selenium.find_elemement_by_id('place_name-5')
-        self.assertEqual('Denver', denver.text)
+        denver = self.selenium.find_elemement_by_id('place_name-5')  # wishlist html 
+        self.assertEqual('Denver', denver.text)  # checking page object element
+
+        self.assertIn('Denver', self.selenium.page_source)  # text on the page is on visited page 
+        self.assertIn('New York', self.selenium.page_source)  # text on the page is on visited page 
+        self.assertIn('Tokyo', self.selenium.page_source)  # text on the page is on visited page 
+
+        # this will error if denver, pk=5 isn't there
+        denver_db = Place.objects.get(pk=5)
+        self.assertEqual('Denver', denver_db.name) 
+        self.assertTrue(denver_db.visited)
