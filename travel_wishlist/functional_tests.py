7fe338fac34d87626ce51586630718ac70f126dc
Dan Smestad <hockeydan25@gmail.com>
2022-04-04 14:18:55 -0500

    added chromedriver edit to import to add .weddriver after from import WebDriver

--- travel_wishlist/functional_tests.py
+++ travel_wishlist/functional_tests.py
@@ -1,6 +1,6 @@
-from selenium.webdriver.chrome import WebDriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-from django.test import LiveServerTestCase
+from django.test import LiveServerTestCase 
 
 from .models import Place
 
@@ -23,7 +23,6 @@ class TitleTest(LiveServerTestCase):
         super().tearDownClass()
 
     def test_title_on_home_page(self):
-
         self.selenium.get(self.live_server_url)
         self.assertIn('Travel Wishlist', self.selenium.title)    
 
