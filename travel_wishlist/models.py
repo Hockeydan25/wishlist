37c00516da433cb28a54ae0e3e3b9857fe8af027
Dan Smestad <hockeydan25@gmail.com>
2022-04-06 14:16:58 -0500

    coments and adds to urls, views, template files, add scecurity for user login to pages visit or visited,added delete option, modify place list methods added to html ask if you want to delete, adding a picture to base home page.

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+"""Create your models here.
+blanks & null = True means doesn't have to be where as if we said False it would require it not be blank.
+setting up the feilds that build  make a place object 
+"""
 class Place(models.Model):
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    notes = models.TextField(blank=True, null=True)
+    date_visited = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)  
+
 
     def __str__(self) -> str:  # string method list places visted not visited. this is visited
-        return f'{self.name} visted? {self.visited}'
+        photo_str = self.photo.url if self.photo else 'no photo'  # if phot or not setup here
+        notes_str = self.notes[100:] if self.notes else 'no notes' # limitefd to 100 text count
+        return f'{self.name} visted? {self.visited} on {self.date_visited}. Notes: {notes_str}. Photo: {photo_str}'
 
 
--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,40 +1,64 @@
+from re import L
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden  # checks only preson logged in can make change.
 
-# Create your views here.
-
+@login_required
 def place_list(request):
+    """ If this is a POST request, the user clicked the Add button in the form. Check if the new place is valid,
+    if so, save the new Place to the database, and redirect to this same pg. This creates a GET request to this 
+    same Route.   
+    If not a POST route, PLace is not valid , diplay a page with a list of places and for to add new Place.          
+    """
 
     if request.method == 'POST':
     #create new place
-        form = NewPlaceForm(request.POST)  # creating from data in the request
-        place = form.save()  # creating a model object from form.
+        form = NewPlaceForm(request.POST)  # creating from data in the request creating a new place
+        place = form.save(commit=False)  # creating a model object from form.
+        place.user = request.user
         if form.is_valid():  # vaildation agaisnt DB constrants.
             place.save()    # saves place to db.
             return redirect('place_list')  # reloads home page.
 
-    places = Place.objects.filter(visited=False).order_by('name')
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()  # used to creat HTML 
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})  #dict data type.
 
 
-
+@login_required
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', {'visited': visited} )
 
-
+@login_required
 def place_was_visited(request, place_pk):
-    if request.method == 'POST':
-        # place = Place.objects.get(pk=place_pk)
+    if request.method == 'POST':        # place = Place.objects.get(pk=place_pk)
         place = get_object_or_404(Place, pk=place_pk)  # add Place object. trys to get object if not found return 404 but keep working
-        place.visited = True
-        place.save()
-
+        if place.user == request.user:
+            place.visited = True
+            place.save()
+        else:
+            return HttpResponseForbidden()
     #return redirect('places_visited')  can e used depending how you want to redirect the user via button objects actions.
-    return redirect('place_list')  # redircts to wishlist places    
+    return redirect('place_list')  # redircts to wishlist places   
 
+
+@login_required
+def place_details(request, place_pk):
+    place =get_object_or_404(Place, pk=place_pk)
+    return render(request, 'travel_wishlist/place_details.html', {'place': place})
+
+
+@login_required
+def delete_place(request, place_pk):
+    place =get_object_or_404(Place, pk=place_pk)
+    if place.user == request.user:
+        place.delete()
+        return redirect('place_list')
+    else:
+        return HttpResponseForbidden()    
 
 def about(request):
     author = 'Dan'
